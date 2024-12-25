45d06e22be1aa23488777340ab0cfdd0e8283195
carrotbat410 <carrotbat410@gmail.com>
2024-12-25 15:17:30 +0900

    2.7) 글로벌 레이아웃 설정하기

--- frontserver1-pagerouter/src/pages/_app.tsx
+++ frontserver1-pagerouter/src/pages/_app.tsx
@@ -1,11 +1,11 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <h2>글로벌헤더</h2>
-    <Component {...pageProps} />
-  </>
+    <GlobalLayout>
+      <Component {...pageProps}/>
+    </GlobalLayout>
   );
 }
